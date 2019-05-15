39d577593ae5e50d16b52e3bc123b7d751ae84ec
John Gellert <gellertjm@gmail.com>
2019-05-14 21:22:22 -0500

    Added additional code using constants with conditionals and tested

--- client.js
+++ client.js
@@ -32,12 +32,145 @@ const employees = [
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+// for (employee of employees) {
+//   console.log(employee);
+// }
 
-// Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
-// This problem is massive! Break the problem down. Use the debugger.
-// What is the fewest lines of code I can write and test to get just a little closer?
 
-// This is not a race. Everyone on your team should understand what is happening.
-// Ask questions when you don't.
+for(employee of employees){
+  console.log(employeeBonus(employee));
+}
 
-console.log( employees );
+// declare employeeBonus function that accepts and employee object
+// creates a new object with the employees bonus
+// @para employeeInfo    an employee object
+// @return               a new object with bonus info
+function employeeBonus(employeeInfoX) {
+  const percentBonus = finalBonus(employeeInfoX);
+  const percentBonusDecimal = percentBonus / 100;
+
+  // creates new employee bonus object
+  let employee = {
+    name: employeeInfoX.name,
+    bonusPercentage: percentBonus + '%',
+    totalCompensation: percentBonusDecimal *  parseInt(employeeInfoX.annualSalary) + parseInt(employeeInfoX.annualSalary),
+    totalBonus: (finalBonus(employeeInfoX) / 100) *   parseInt(employeeInfoX.annualSalary)
+  }
+  return employee;
+}
+
+function finalBonus(employeeInfoY) {
+  let percent = 0;
+  // bonus percent based on review
+  const noBonus = employeeInfoY.reviewRating <= 2;
+  const fourPercent = employeeInfoY.reviewRating === 3;
+  const sixPercent = employeeInfoY.reviewRating === 4;
+  const tenPercent = employeeInfoY.reviewRating === 5;
+
+  // bonus percent based on tennur greater than 15 years
+  const addFive = employeeInfoY.employeeNumber.length === 4;
+
+  // bonus percent based on salary cap of 65,000
+  const overSalaryCap = employeeInfoY.annualSalary > 65000;
+
+  // bonus percent based on bonus cap of 13%
+  const overBonusCap = percent > 13;
+
+  // set percent based on employee review rating
+  if (noBonus) {
+    percent = 0;
+  }
+  else if(fourPercent){
+    percent = 4;
+  }
+  else if (sixPercent){
+    percent = 6;
+  }
+  else if (tenPercent){
+    percent = 10;
+  }
+
+   // add five percent if the employee has been with the company for ober 15 years
+  if (addFive){
+    percent += 5;
+  }
+
+  // set percent to 1 if current salary is over 65000
+  if(overSalaryCap){
+    percent = 1;
+  }
+
+  // set percent to 13 if greater than 13
+  if (overBonusCap){
+    percent = 13;
+  }
+
+  return percent;
+} // end finalBonus
+
+
+
+
+
+// initial code
+// function employeeBonus(employee) {
+//   percentCalc(employee);
+//   let emp = {
+//     name: employee.name,
+//     bonusPercentage: finalBonus(employee),
+//     totalCompensation: employee.annualSalary,
+//     totalBonus: employee.bonusPercentage
+//   }
+//   return emp;
+// }
+
+// let bonusPercent = 0;
+
+// function percentCalc(employee) {
+//   if (employee.reviewRating < 2) {
+//     bonusPercent;
+//   }
+//   else if (employee.reviewRating === 3) {
+//     bonusPercent = 4;
+//   }
+//   else if (employee.reviewRating === 4) {
+//     bonusPercent = 6;
+//   }
+//   else if (employee.reviewRating === 5) {
+//     bonusPercent = 10;
+//   }
+// }
+
+// function finalBonus(employee) {
+//   if (employee.employeeNumber.length === 4) {
+//     bonusPercent += 5;
+//     if (employee.annualSalary > 65000) {
+//       bonusPercent = 1;
+//     }
+
+//   }
+//   if (bonusPercent > 13) {
+//     bonusPercent = 13;
+//   }
+//   return bonusPercent;
+// }
+
+
+
+
+
+
+
+// console.log(employeeBonus(employees[0]));
+// console.log(employeeBonus(employees[1]));
+// console.log(employeeBonus(employees[2]));
+
+
+// // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
+// // This problem is massive! Break the problem down. Use the debugger.
+// // What is the fewest lines of code I can write and test to get just a little closer?
+
+// // This is not a race. Everyone on your team should understand what is happening.
+// // Ask questions when you don't.
+
+// //console.log( employees );
